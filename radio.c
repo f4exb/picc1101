c44e8360ea9b9b298ba628009ca1dda2ed55904d
Edouard Griffiths <f4exb06@gmail.com>
2015-02-13 02:13:30 +0100

    corrected rate related words calculation

--- radio.c
+++ radio.c
@@ -91,10 +91,10 @@ static void get_rate_words(rate_t rate_code, modulation_t modulation_code, radio
         deviat *=2;
     }
 
-    radio_parms->drate_e = (uint8_t) (floor(log2( drate*(1<<20)) / radio_parms->f_xtal ));
+    radio_parms->drate_e = (uint8_t) (floor(log2( drate*(1<<20) / radio_parms->f_xtal )));
     radio_parms->drate_m = (uint8_t) (((drate*(1<<28)) / (radio_parms->f_xtal * (1<<radio_parms->drate_e))) - 256);
 
-    radio_parms->deviat_e = (uint8_t) (floor(log2( deviat*(1<<13)) / radio_parms->f_xtal ));
+    radio_parms->deviat_e = (uint8_t) (floor(log2( deviat*(1<<13) / radio_parms->f_xtal )));
     radio_parms->deviat_m = (uint8_t) (((deviat*(1<<17)) / (radio_parms->f_xtal * (1<<radio_parms->deviat_e))) - 8);
 }
 
