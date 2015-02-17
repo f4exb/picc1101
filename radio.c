7ff38dac26d099a9e8505e95dcef582a7cc507a4
Edouard Griffiths <f4exb06@gmail.com>
2015-02-17 02:28:16 +0100

    debug message 2

--- radio.c
+++ radio.c
@@ -83,7 +83,7 @@ void int_packet_simple(void)
     uint8_t x_byte, int_packet, rx_bytes, rssi_dec, crc_lqi;
     int i;
 
-    fprintf(stderr, "GDO0 interrupt\n");
+    fprintf(stderr, "GDO0 interrupt %d\n", radio_int_data->packet_limit);
 
     PI_CC_SPIReadStatus(radio_int_data->spi_parms, PI_CCxxx0_PKTSTATUS, &x_byte); // sense interrupt lines
     int_packet = x_byte & 0x01; // GDO0 (& 0x01) packet interrupt
