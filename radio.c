b976f2ef8fafab96b50d19cef0cc29bab462e840
Edouard Griffiths <f4exb06@gmail.com>
2015-02-23 00:29:48 +0100

    compile fix

--- radio.c
+++ radio.c
@@ -163,7 +163,7 @@ void int_packet(void)
 void int_threshold(void)
 // ------------------------------------------------------------------------------------------------
 {
-    uint8_t i, bytes_to_send, x_byte;
+    uint8_t i, int_line, bytes_to_send, x_byte;
 
     int_line = digitalRead(WPI_GDO2); // Sense interrupt line to determine if it was a raising or falling edge
 
