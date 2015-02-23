b7712150b808f609adba5215900a78e63e9f5a1b
Edouard Griffiths <f4exb06@gmail.com>
2015-02-23 13:04:28 +0100

    undid wait before sending next packet when in variable length mode in Tx test routine

--- radio.c
+++ radio.c
@@ -1033,11 +1033,6 @@ int radio_transmit_test_int(spi_parms_t *spi_parms, arguments_t *arguments)
     {
         radio_send_packet(spi_parms, arguments, arguments->test_phrase, strlen(arguments->test_phrase));
         packets_sent++;
-        
-        if (arguments->variable_length)
-        {
-        	usleep(100 * radio_int_data.wait_us); // wait before sending next packet when in variable length mode
-        }
     } 
 }
 
