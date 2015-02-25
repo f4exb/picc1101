782619f5128409586bcdb0ca5a763cf55127f826
Edouard Griffiths <f4exb06@gmail.com>
2015-02-25 17:14:52 +0100

    debug concatenated KISS block received on serial at level 3 only

--- kiss.c
+++ kiss.c
@@ -164,12 +164,13 @@ void kiss_run(serial_t *serial_parms, spi_parms_t *spi_parms, arguments_t *argum
             	uint8_t *kiss_fend, *kiss_frame = read_buffer;
 
                 verbprintf(2, "Concatenated KISS block encountered\n");
-                print_block(2, read_buffer, read_bytes);
-                verbprintf(2, "...\n");
+                print_block(3, read_buffer, read_bytes);
+                verbprintf(3, "...\n");
 
                 while ((kiss_fend = kiss_tok(kiss_frame, (uint8_t *) read_buffer + read_bytes)))
                 {
-	                print_block(2, kiss_frame, kiss_fend - kiss_frame + 1);
+                	verbprintf(2, "Processing KISS block of %d bytes\n", kiss_fend - kiss_frame + 1);
+	                print_block(3, kiss_frame, kiss_fend - kiss_frame + 1);
                 	radio_send_packet(spi_parms, arguments, kiss_frame, kiss_fend - kiss_frame + 1);
                 	kiss_frame = kiss_fend + 1;
                 }
