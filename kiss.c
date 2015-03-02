fea9c8674468e55abeb765bc1fe1b68ee6f341c6
Edouard Griffiths <f4exb06@gmail.com>
2015-03-02 19:12:18 +0100

    activation of cumulative Rx and Tx

--- kiss.c
+++ kiss.c
@@ -185,7 +185,7 @@ void kiss_run(serial_t *serial_parms, spi_parms_t *spi_parms, arguments_t *argum
 // ------------------------------------------------------------------------------------------------
 {
     static const size_t   bufsize = RADIO_BUFSIZE;
-    uint32_t timeout_value = arguments->packet_length / 2;
+    uint32_t timeout_value = arguments->packet_length / 10;
     uint8_t rx_buffer[bufsize], tx_buffer[bufsize];
     uint8_t rtx_toggle; // 1:Tx, 0:Rx
     uint8_t rx_trigger; 
