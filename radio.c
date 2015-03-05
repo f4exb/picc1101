de8e4d1027ab4a201998cd8bd5f836ad85e5ad0e
Edouard Griffiths <f4exb06@gmail.com>
2015-03-05 23:21:56 +0100

    Make more accurate packet time calculation and corrected display

--- radio.c
+++ radio.c
@@ -944,9 +944,9 @@ void print_radio_parms(radio_parms_t *radio_parms)
         ((radio_parms->f_xtal/1e3) / (1<<17)) * (8 + radio_parms->deviat_m) * (1<<radio_parms->deviat_e), radio_parms->deviat_m, radio_parms->deviat_e);
     fprintf(stderr, "Packet length ..........: %d bytes\n",
         radio_parms->packet_length);
-    fprintf(stderr, "Byte time ..............: %d us\n",
-        radio_parms->packet_length * ((uint32_t) radio_get_byte_time(radio_parms)));
     fprintf(stderr, "Packet time ............: %d us\n",
+        (uint32_t) (radio_parms->packet_length * radio_get_byte_time(radio_parms));
+    fprintf(stderr, "Byte time ..............: %d us\n",
         ((uint32_t) radio_get_byte_time(radio_parms)));
 }
 
